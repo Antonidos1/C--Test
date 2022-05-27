949a8f2a6dbb535d4768010417230d2f5546ad57
Antonidos1 <mikhail-andreev-2001@mail.ru>
2022-05-27 11:34:40 +0400

    test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //10 10 10 10 10
+            List<Action> actions = new List<Action>();
+            for (var count = 0; count < 10; count++)
+            {
+                actions.Add(() => Console.WriteLine(count));
+
+            }
+            foreach (var action in actions)
+            {
+                action();
+            }
+        }
+    }
+}
